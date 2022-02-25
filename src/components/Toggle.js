cad095c9d3ad5cbef8ba8c43180837f1bea8bde0
Kristina Peru <krissy.germanov@gmail.com>
2022-02-25 00:58:30 -0700

    Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  let [isOn, setIsOn] = useState(false)
+  function hancleClick() {
+    setIsOn(isOn = !isOn)
+  }
+
+  const color = isOn ? "red" : "white"
+
+  return <button style={{ background: color }} onClick={hancleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
